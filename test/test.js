6d3d9809cae15410abe00068a820a0b23267f423
zahiruldu <zahirul.haque@aurea.com>
2019-09-14 00:45:57 +0600

    config api tests

--- test/test.js
+++ test/test.js
@@ -33,3 +33,43 @@ describe('/api', () => {
         });
   });
 });
+
+describe('/api/configs', () => {
+  it('it should return all configs of sites ', (done) => {
+    chai.request(server)
+        .get('/api/configs')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body[0].should.have.property('name');
+          res.body[0].should.have.property('uri');
+          res.body[0].should.have.property('config');
+          res.body[0].should.have.property('tags');
+          res.body[0].should.have.property('content_type');
+          res.body[0].should.have.property('state');
+          res.body[0].should.have.property('schedule');
+          res.body[0].should.have.property('ts');
+          done();
+        });
+  });
+
+  it('it should return a config of sample url ', (done) => {
+    let url = 'https://wildbit.com/jobs';
+    chai.request(server)
+        .get('/api/configs')
+        .query({url: url})
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('uri');
+          res.body.should.have.property('config');
+          res.body.should.have.property('tags');
+          res.body.should.have.property('content_type');
+          res.body.should.have.property('state');
+          res.body.should.have.property('schedule');
+          res.body.should.have.property('ts');
+          done();
+        });
+  });
+});
